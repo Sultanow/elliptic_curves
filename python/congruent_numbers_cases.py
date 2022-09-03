d50c30fd37283520f6be586e72256c6cf089af1e
Sultanow <sultanow@outlook.com>
2022-09-03 14:50:51 +0200

    update

--- python/congruent_numbers_cases.py
+++ python/congruent_numbers_cases.py
@@ -1,16 +1,19 @@
 # https://www.overleaf.com/read/prsvxqtdwthm
-import multiprocessing as mp, traceback, math
+import multiprocessing as mp, traceback, math, threading
 
 limA = 1000
 limB = 60
 
-def PQs():
+def Factor(n):
     import sympy
+    fs = []
+    for k, v in sympy.factorint(n).items():
+        fs.extend([k] * v)
+    return sorted(fs)
+
+def PQs():
     for n in range(1 << 20):
-        fs = []
-        for k, v in sympy.factorint(n).items():
-            fs.extend([k] * v)
-        fs = sorted(fs)
+        fs = Factor(n)
         if len(fs) != 2:
             continue
         if fs[0] == fs[1]:
@@ -40,22 +43,46 @@ def Task(ipq, *, ab = [(a, b) for a in range(-limA + 1, limA) for b in range(1, 
     except:
         return {'error': traceback.format_exc()}
 
+def Log(*pargs, _state = {'lock': threading.Lock(), 'file': None}, **nargs):
+    with _state['lock']:
+        if _state['file'] is None:
+            _state['file'] = open('ec_pq_find_rational_xy_conditions_log.txt', 'a', encoding = 'utf-8')
+        print(*pargs, **nargs, file = _state['file'], flush = True)
+
 def Main():
+    import sympy
     with mp.Pool(mp.cpu_count()) as pool:
-        k, kc, avgA, avgB, maxA, maxB = 0, 0, 0, 0, 0, 0
+        cnt, scnt, k, kc, avgA, avgB, maxA, maxB, primeB, primeOddB = [0] * 10
+        unprimeBs = {}
         for e in pool.imap(Task, enumerate(PQs())):
             if type(e) is dict:
                 assert False, e['error']
             (i, p, q), cases = e
+            unprimeB_logged = False
             for _, v in cases.items():
                 for a, b in v:
                     maxA = max(abs(a), maxA)
                     maxB = max(b, maxB)
                     avgA += abs(a)
                     avgB += b
+                    primeB += int(sympy.isprime(b))
+                    t = b
+                    while t > 0 and (t & 1) == 0:
+                        t >>= 1
+                    if t == 1 or sympy.isprime(t):
+                        primeOddB += 1
+                    else:
+                        fst = tuple(Factor(t))
+                        unprimeBs[fst] = unprimeBs.get(fst, 0) + 1
+                        if not unprimeB_logged:
+                            Log('un-prime-B', b, f'({Factor(b)})', f'p={p},q={q}', f' cases {i}:', cases)
+                            unprimeB_logged = True
                     k += abs(a) / b
                     kc += 1
-            print(f'{i:>5}: {p:>5}, {q:>5}', '   ', *((cases, '     avg(a/b)', round(k / max(1, kc), 1), 'maxA', maxA, 'avgA', round(avgA / kc, 1), 'maxB', maxB, 'avgB', round(avgB / kc, 1)) if cases else ()))
+            cnt += 1
+            if cases:
+                scnt += 1
+            print(f'{i:>5}: {p:>5}, {q:>5}', '   ', *((cases, f'     solved {round(scnt / cnt * 100, 1)}%', 'avg(a/b)', round(k / max(1, kc), 1), 'maxA', maxA, 'avgA', round(avgA / kc, 1), 'maxB', maxB, 'avgB', round(avgB / kc, 1), 'non-primeB', kc - primeB, 'prime-B', primeB, f'({round(primeB / kc * 100, 1)}%)', 'prime-odd-B', primeOddB, f'({round(primeOddB / kc * 100, 1)}%)', 'un-prime-Bs', dict(sorted(unprimeBs.items(), key = lambda e: e[0]))) if cases else ()), flush = True)
 
 if __name__ == '__main__':
     Main()
